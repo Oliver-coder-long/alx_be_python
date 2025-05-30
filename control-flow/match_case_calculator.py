8c75f57946615ed5aa274b5108c64e8d7b2b77ad
Oliver-coder-long <olishema11@gmail.com>
2025-05-30 12:35:35 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,10 +3,10 @@ num2 = input ("Enter the second number: ")
 operation = input ("Choose the operation (+, -, *, /): ")
 match operation:
   case "+":
-    print ("The result is {num1 + num2}")
+    print (f"The result is {num1 + num2}")
   case "-":
-    print ("The result is {num1 - num2}")
+    print (f"The result is {num1 - num2}")
   case "*":
-    print ("The result is {num1 * num2}")
+    print (f"The result is {num1 * num2}")
   case "/":
-    print ("The result is {num1/num2}" if num2 > 0)
+    print (f"The result is {num1/num2}" if num2 > 0)
