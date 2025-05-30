7da89dcb7af4e2787a0e4c30b85788d086d56ff5
Oliver-coder-long <olishema11@gmail.com>
2025-05-30 16:59:42 +0300

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,7 @@
 size = int(input("Enter the size of the pattern: "))
 row = 1
 while row <= size:
-  for rows in range(1, size + 1):
+  for rows in range(1, size):
     print ("*", end="")
   print ()
   row += 1
