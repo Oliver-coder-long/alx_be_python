1140786690f57ff62bb77ab9fe56a063a820e983
Oliver-coder-long <olishema11@gmail.com>
2025-05-30 17:30:45 +0300

    Create daily_reminder.py

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,9 @@
+task = input("Enter task description:")
+priority = input("Enter task priority:(high, medium, low)")
+time_bound = input("Is it time_bound?(yes/no):")
+match priority:
+  case "high":
+
+  case "medium":
+
+  case "low":
