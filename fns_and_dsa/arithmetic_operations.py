6062807ed32cd49312dde67aa4b75dda7ae3c208
Oliver-coder-long <olishema11@gmail.com>
2025-06-06 16:12:59 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,7 +5,7 @@ def perform_operation(num1, num2, operation):
     result = num1 - num2
   elif operation == "multiply":
     result = num1 * num2
-  elif operation == "divide" and num2 != 0:
+  elif operation == "divide" if num2 != 0:
     result = num1/num2
   return result
     
