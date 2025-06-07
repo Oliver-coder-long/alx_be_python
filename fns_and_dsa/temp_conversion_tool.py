9cfbdd64396c5fc0eb01bfb7fdef82a865838fec
Oliver-coder-long <olishema11@gmail.com>
2025-06-07 16:42:21 +0300

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,11 +4,11 @@ def convert_to_celsius(fahrenheit):
   return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 def convert_to_fahrenheit(celcius):
   return celcius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
-temp = int(input("enter the temperature:"))
-temp_version = input("is it in celcius or fahrenheit?(Cel/fahr):")
-if temp_version == "cel":
+temp = int(input("Enter the temperature to convert:"))
+temp_version = input("Is this temperature in Celsius or Fahrenheit? (C/F):")
+if temp_version == "C":
   convert_to_fahrenheit(temp)
-elif temp_version == "fahr":
+elif temp_version == "F":
   convert_to_celcius(temp)
 else:
   print("Enter a valid temperature unit from the two options.")
