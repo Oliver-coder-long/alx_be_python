0e1c66e7ef0024cc51387a5208abac71608f4ab6
Oliver-coder-long <olishema11@gmail.com>
2025-06-16 20:39:41 +0300

    Create bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,14 @@
+class BankAccount:
+  def _init_(self, account_balance):
+    self.account_balance = account_balance
+    self.initial_balance = 0
+  def deposit(amount):
+    return account_balance += amount
+  def withdraw(amount):
+    if amount >= account_balance:
+      return account_balance -= amount
+    else:
+      return false
+      print("You have insufficient funds")
+  def display_balance():
+    print (f"Current balance: ${amount}")
