ac6de94489ae1d18a987960203b0ac7ec0358976
Oliver-coder-long <olishema11@gmail.com>
2025-06-20 11:35:40 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,10 +6,12 @@ class BankAccount:
         account_balance += amount
         print(f"Deposited: ${self.amount}")
     def withdraw(self, amount):
-        if self.balance >= amount:
+        if 0 < amount <= self.account_balance:
             account_balance -= amount
             print(f"Withdrew: ${self.amount}")
+            return True
         else:
            print("Insufficient funds")
+           return False
     def display_balance(self):
-        print(f"[Current balance: ${balance}]")
+        print(f"[Current balance: ${self.account_balance}]")
